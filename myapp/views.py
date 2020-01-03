b1c47c760e9900a0363ad4d1b52643f08280d8c3
Roy Miles <rm5118@ic.ac.uk>
2020-01-03 17:47:03 +0100

    Upload nearly working.

--- myapp/views.py
+++ myapp/views.py
@@ -5,27 +5,34 @@ import tensorflow_hub as hub
 from django.http import HttpResponseRedirect
 from django.template import Context, Template
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+import uuid
 
 
 def collage(request):
-    content_image = load_img("C:\\Users\\Roy\\PycharmProjects\\StyleTransfer\\dog.jpg")
-    style_image = load_img("C:\\Users\\Roy\\PycharmProjects\\StyleTransfer\\van_gogh.jpg")
+    content_image = load_img("myapp/static/content-images/dog.jpg")
+    style_image = load_img("myapp/static/style-images/van_gogh.jpg")
 
     hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/1')
     stylized_image = hub_module(tf.constant(content_image), tf.constant(style_image))[0]
     out_image = tensor_to_image(stylized_image)
     out_image.save("output.jpg")
 
-    return HttpResponse("Done")
+    return render(request, 'collage.html', {'collage_link': "output.jpg"})
 
 
 def upload(request):
-    if request.method == 'POST':
+    print(request)
+    if request.method == 'POST' and request.FILES['image']:
         """ Handle the file upload request """
+        image = request.FILES['image']
+        image.name = uuid.uuid1()
+        fs = FileSystemStorage()
+
+        filename = fs.save("myapp/static/content-images/" + image.name, image)
+        uploaded_file_url = fs.url(filename)
+
         return HttpResponseRedirect('/collage')
     else:
         """ Display the upload form """
-        template = Template("My name is {{ my_name }}.")
-        context = Context({"my_name": "Adrian"})
-        template.render(context)
-        return render(request, 'example.html', {'title': "xdfg", 'cal': "dfgdfg"})
+        return render(request, 'upload.html')
